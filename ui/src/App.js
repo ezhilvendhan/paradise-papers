2a48e8410172aab582a943c9a9b9a7b7da3f34a2
Ezhil Vendhan <uibuddha@gmail.com>
2018-09-08 10:57:10 +0800

    Fix App.js/css

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserList from './UserList';
+import EntityVis from './EntityVis';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={process.env.PUBLIC_URL + '/img/grandstack.png'} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to GRANDstack</h1>
+          <h1 className="App-title">Paradise Papers</h1>
         </header>
         
-        <UserList />
+        <EntityVis />
       </div>
     );
   }
